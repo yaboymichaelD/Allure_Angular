632ded5cae6399329a3033e3569f9c72a0e1c3c6
yaboymichaeld <yaboymichaeld@gmail.com>
2018-10-18 10:53:20 +0100

    Work done on the 18th

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const ctrlMain = require('../controllers/main'); 
-/* GET home page. */
-router.get('/', ctrlMain.index); 
+const ctrlLocations = require('../controllers/locations'); 
+const ctrlOthers = require('../controllers/others');
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
+
+
 
 
 
